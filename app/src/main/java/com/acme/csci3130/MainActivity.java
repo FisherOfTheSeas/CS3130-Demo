50cc71a2f3d79422be9670000307fd89ccf79cb1
micha <michael_fisher_15@hotmail.com>
2017-05-08 16:38:29 -0300

    Added a second textfield and a button.

--- app/src/main/java/com/acme/csci3130/MainActivity.java
+++ app/src/main/java/com/acme/csci3130/MainActivity.java
@@ -2,12 +2,32 @@ package com.acme.csci3130;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.EditText;
+import android.view.View;
 
 public class MainActivity extends Activity {
+
+private TextView tv;
+
+private EditText et;
+
+private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tv = (TextView) findViewById(R.id.helloText);
+        et = (EditText) findViewById(R.id.editText);
+        //Sfinal String str = et.getText().toString();
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                tv.setText(et.getText().toString());
+            }
+        });
     }
 }
